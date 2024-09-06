84dfb6e1b755494e4d6a8975ec84795b93fc8c88
AlvaroContreras13 <ac2021070020@virtual.upt.pe>
2024-09-05 19:30:14 -0500

    enviroment variable #1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.example.com'; // Reemplaza con la URL de tu API
+const backend_url=process.env.API_URL;
 
 // funcion para enviar los datos de login a la API
 export const login = (username, password) => {
   // envia una solicitud POST a la ruta de login de la API con el nombre de usuario y la contraseña
-  return axios.post(`${API_URL}/login`, { username, password });
+  return axios.post(`${backend_url}/login`, { username, password });
 };
